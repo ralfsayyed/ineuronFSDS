0ff7c16b5582b0d23670c77085c0b6df3c43404d
Nayeem Sayyed <ralfsayyed@gmail.com>
2022-07-07 15:53:23 +0530

    Add files via upload

---
+++ ProgrammingAssignment1.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python
+# coding: utf-8
+1. 
+# In[1]:
+
+
+print("Hello python")
+
+
+# In[ ]:
+
+
+2. 
+
+
+# In[8]:
+
+
+a= 2
+b= 5
+sum = a+b
+print(f'This sum of {a} & {b} is {sum}')
+
+
+# 3 . Write a Python program to find the area of a triangle?
+
+# In[11]:
+
+
+h=int(input("Enter the hight of the triangle"))
+b=int(input("Enter the base of the triable "))
+area =  1/2*h*b
+print(f"The area of Triangle is : {area}")
+
+
+# 4. Write a Python program to swap two variables?
+
+# In[12]:
+
+
+a=input("Enter the value of a")
+b=input("enter the value of b")
+c=a
+a=b
+b=c
+print(f"The Value of a is{a} & b is {b}")
+
+
+# 5. Write a Python program to generate a random number?
+
+# In[17]:
+
+
+import random
+print(random.randint(0,9))
+
+
+# In[ ]:
+
+
+
+
